a92d191126a47e3a91384c69e0bbee51a4770003
MarinaGolacheva <marina.golacheva@gmail.com>
2026-05-26 20:15:30 +0200

    Parametrized method is added

--- tests/usePageObjects.spec.ts
+++ tests/usePageObjects.spec.ts
@@ -1,15 +1,24 @@
 import { test, expect } from "@playwright/test"
 import { NavigationPage } from '../page-objects/navigationPage'
+import { FormLayoutsPage } from "../page-objects/formLayoutsPage"
 
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200/')
 })
 
 test('navigate to form page', async ({ page }) => {
-const navigateTo = new NavigationPage (page)
-await navigateTo.formLayoutsPage()
-await navigateTo.datepickerPage()
-await navigateTo.smartTablePage()
-await navigateTo.toastrPage()
-await navigateTo.tooltipPage()
+    const navigateTo = new NavigationPage(page)
+    await navigateTo.formLayoutsPage()
+    await navigateTo.datepickerPage()
+    await navigateTo.smartTablePage()
+    await navigateTo.toastrPage()
+    await navigateTo.tooltipPage()
+})
+
+test('parametrized methods', async ({ page }) => {
+    const navigateTo = new NavigationPage(page)
+    const onFormLayoutPage = new FormLayoutsPage(page)
+
+    await navigateTo.formLayoutsPage()
+    await onFormLayoutPage.submitUsingTheGridFormWithCredentialsAndSelectOption('test@test.com', 'Welcome1', 'Option 2')
 })
